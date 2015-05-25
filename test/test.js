8be4a2aa8b9d1946dd67aef09317f0a31a018260
Sann-Remy Chea <sannremy.chea@gmail.com>
2015-05-25 21:22:33 +0200

    Add 2 tests

--- test/test.js
+++ test/test.js
@@ -1,27 +1,109 @@
-QUnit.test("Pulse returning types", function(assert) {
+QUnit.test("Pulse options returning types", function(assert) {
+
+    assert.ok(function() {
+        var pulse = new Pulse();
+        return typeof pulse.options.onComplete === 'function';
+    }, 'onComplete is a function');
+
+    assert.ok(function() {
+        var pulse = new Pulse();
+        return typeof pulse.options.onRequestProgress === 'function';
+    }, 'onRequestProgress is a function');
+
+    assert.ok(function() {
+        var pulse = new Pulse();
+        return typeof pulse.options.onRequestSuccess === 'function';
+    }, 'onRequestSuccess is a function');
+
+    assert.ok(function() {
+        var pulse = new Pulse();
+        return typeof pulse.options.onRequestAbort === 'function';
+    }, 'onRequestAbort is a function');
+
+    assert.ok(function() {
+        var pulse = new Pulse();
+        return typeof pulse.options.onRequestError === 'function';
+    }, 'onRequestError is a function');
+
+    assert.ok(function() {
+        var pulse = new Pulse();
+        return typeof pulse.options.convertToMilliseconds === 'boolean';
+    }, 'convertToMilliseconds is a boolean');
+
+    assert.ok(function() {
+        var pulse = new Pulse();
+        return typeof pulse.options.removeDuplicates === 'boolean';
+    }, 'removeDuplicates is a boolean');
+});
+
+QUnit.test("Pulse options callbacks", function(assert) {
+
     assert.ok(function() {
         var pulse = new Pulse();
         return pulse.loadBufferFromURI("assets/song.mp3");
-    }, 'Web Audio API is supported');
+    }, 'loadBufferFromURI works as expected and Web Audio API is supported by the browser');
+
+    // var done = assert.async();
+    // var pulse = new Pulse({
+    //     onRequestSuccess: function(event, request) {
+    //         assert.ok(function() {
+    //             return true;
+    //         }, 'ok');
+    //         done();
+    //     }
+    // });
+
+    // pulse.loadBufferFromURI("assets/song.mp3");
+
 });
 
-QUnit.test("Pulse", function(assert) {
-    var done = assert.async();
+QUnit.test( "assert.async() test", function( assert ) {
+  var done = assert.async();
 
-    var pulse = new Pulse();
-    pulse.onComplete = function(event, pulse) {
-        console.log(typeof pulse.renderedBuffer);
-        assert.equal(typeof pulse.renderedBuffer, '', '');
+  var input = document.activeElement;
 
-        var extrapolatedPeaks = pulse.getExtrapolatedPeaks(
-            pulse.renderedBuffer,
-            pulse.significantPeaks,
-            pulse.beat
-        );
+  var requestSuccess = function(event, request) {
+    assert.equal( document.activeElement, input, "Input was focused" );
+    done();
+  };
+
+  var pulse = new Pulse({
+    onRequestSuccess: function(event, request) {
+        assert.equal( document.activeElement, input, "Input was focused" );
         done();
-    };
-    pulse.loadBufferFromURI("assets/song.mp3");
+      }
+  });
+
+  pulse.loadBufferFromURI("assets/song.mp3");
+
+
+  // var input = document.activeElement;
+  // setTimeout(function() {
+  //   assert.equal( document.activeElement, input, "Input was focused" );
+  //   done();
+  // });
 });
+
+// QUnit.test( "a Promise-returning test", function( assert ) {
+//  var done = assert.async();
+
+//  setTimeout(function() {
+//   var pulse = new Pulse();
+//     pulse.onComplete = function(event, pulse) {
+//         var extrapolatedPeaks = pulse.getExtrapolatedPeaks(
+//             pulse.renderedBuffer,
+//             pulse.significantPeaks,
+//             pulse.beat
+//         );
+
+//         assert.ok(function() {
+//             return typeof pulse.renderedBuffer === 'object';
+//         }, 'pulse.renderedBuffer is an object');
+//         done();
+//     };
+//     pulse.loadBufferFromURI("assets/song.mp3");
+//   }, 0);
+// });
 
 /*var pulse = new Pulse({
     onComplete: function(event, pulse) {
